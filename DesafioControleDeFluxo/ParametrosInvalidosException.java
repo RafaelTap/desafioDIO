a6cb708646db1b7db5f128adae0a7546f8151c10
Rafael Taparica <rafael.taparica@protonmail.com>
2023-08-31 14:50:42 -0300

    Add files via upload

---
+++ DesafioControleDeFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,17 @@
+package DesafioControleDeFluxo;
+
+public class ParametrosInvalidosException {
+
+
+	public ParametrosInvalidosException() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "parâmetro segundo deve ser maior que o primeiro";
+	}
+	
+	
+
+}
